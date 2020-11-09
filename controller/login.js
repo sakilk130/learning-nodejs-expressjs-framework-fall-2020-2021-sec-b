3de66bcf90dbc610afe01cc9fa592cb06c78be8f
Sakil Khan <sakilk130@gmail.com>
2020-11-10 03:28:31 +0600

    login database add

--- controller/login.js
+++ controller/login.js
@@ -7,13 +7,40 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  if (req.body.username == req.body.password) {
-    //req.session.uname =  req.body.username;
-    res.cookie('uname', req.body.username);
-    res.redirect('/home');
-  } else {
-    res.redirect('/login');
-  }
+  var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'sakil',
+    password: 'rREPa3GP54',
+    database: 'node1',
+  });
+
+  connection.connect(function (err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+  });
+  var sql =
+    "select * from user where username='" +
+    req.body.username +
+    "' and password='" +
+    req.body.password +
+    "'";
+
+  connection.query(sql, function (error, results) {
+    if (results.length > 0) {
+      res.cookie('uname', req.body.username);
+      res.redirect('/home');
+    } else {
+      res.redirect('/login');
+    }
+  });
+
+  connection.end(function (err) {
+    console.log('connection closed!');
+  });
 });
 
 module.exports = router;
