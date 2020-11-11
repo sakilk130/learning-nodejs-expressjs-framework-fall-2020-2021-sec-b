8a21e58a1fb682e25df4dca15b7c3503f3193631
Sakil Khan <sakilk130@gmail.com>
2020-11-11 13:58:52 +0600

    add lab-performance

---
+++ lab-performance/controller/user.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const userModel	= require.main.require('./models/userModel');
+const router = express.Router();
+
+router.get('/create', (req, res)=>{
+	res.render('user/create'); 
+})
+
+router.post('/create', (req, res)=>{
+
+	var user = {
+		username: 	req.body.username,
+		password: 	req.body.password,
+		type	: 	req.body.type
+	};
+
+	userModel.insert(user, function(status){
+		if(status){
+			res.redirect('/home/userlist');
+		}else{
+			res.redirect('user/create');
+		}
+	});
+})
+
+
+router.get('/edit/:id', (req, res)=>{
+
+	//var data = req.params.id;
+	//res.send(data);
+	var user ={
+		id: req.params.id,
+		username: 'alamin',
+		password: '123',
+		email: 'alamin@gmail.com',
+		dept: 'CS'
+	};
+
+	res.render('user/edit', user);
+})
+
+
+router.post('/edit/:id', (req, res)=>{
+
+	//req.body.username
+	//req.body.email
+	//req.body.password
+	//req.body.dept
+
+	res.redirect('/home/userlist');
+})
+
+router.get('/delete/:id', (req, res)=>{
+	var user ={
+		id: req.params.id,
+		username: 'alamin',
+		password: '123',
+		email: 'alamin@gmail.com',
+		dept: 'CS'
+	};
+
+	res.render('user/delete', user);
+})
+
+router.post('/delete/:id', (req, res)=>{
+	
+	//delete from DB
+	res.redirect('/home/userlist');
+})
+
+module.exports = router;
+
+
+//validation -> express-validator (https://www.npmjs.com/package/express-validator)
+//file upload -> express-fileupload (https://www.npmjs.com/package/express-fileupload)
---
+++ my-work/app.js
@@ -0,0 +1,42 @@
+//declaration
+const express = require('express');
+const bodyParser = require('body-parser');
+const exSession = require('express-session');
+const cookieParser = require('cookie-parser');
+const login = require('./controller/login');
+const home = require('./controller/home');
+const logout = require('./controller/logout');
+const user = require('./controller/user');
+const app = express();
+const index = require('./controller/index');
+
+//config
+app.set('view engine', 'ejs');
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  exSession({
+    secret: 'my secret value',
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+app.use(cookieParser());
+
+app.use('/', index);
+app.use('/home', home);
+app.use('/login', login);
+app.use('/logout', logout);
+app.use('/user', user);
+
+//route
+app.get('*', (req, res) => {
+  res.send('<h1>404 Not Found</h1>');
+});
+
+//server startup
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, (error) => {
+  console.log(`express server started at ${PORT}`);
+});
