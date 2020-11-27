7d565787a42e707a583f972acb38a6e8174266d7
Sakil Khan <sakilk130@gmail.com>
2020-11-27 14:56:59 +0600

    General User-->home,profile,change_password

--- mid-assignment/controller/user/home.js
+++ mid-assignment/controller/user/home.js
@@ -4,10 +4,82 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   if (req.cookies['uname'] != null) {
-    res.render('user/home');
+    const username = req.cookies['uname'];
+    travel_guide_model.getByUsername(username, function (results) {
+      // console.log(results);
+      res.render('user/home', { user: results });
+    });
   } else {
     res.redirect('/login');
   }
+});
+
+router.get('/profile', (req, res) => {
+  if (req.cookies['uname'] != null) {
+    const username = req.cookies['uname'];
+    travel_guide_model.getByUsername(username, function (results) {
+      // console.log(results);
+      res.render('user/profile', { user: results });
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
+
+router.post('/profile', (req, res) => {
+  const user = {
+    uname: req.cookies['uname'],
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+  console.log(user);
+  travel_guide_model.updateProfile(user, function (status) {
+    if (status) {
+      res.redirect('/user');
+    } else {
+      res.send('Not Update');
+    }
+  });
+});
+
+router.get('/change', (req, res) => {
+  if (req.cookies['uname'] != null) {
+    const username = req.cookies['uname'];
+    travel_guide_model.getByUsername(username, function (results) {
+      // console.log(results);
+      res.render('user/change', { user: results });
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
+
+router.post('/change', (req, res) => {
+  const change = {
+    uname: req.cookies['uname'],
+    old_pass: req.body.old_pass,
+    new_pass: req.body.new_pass,
+    c_new_pass: req.body.c_new_pass,
+  };
+  travel_guide_model.getByUsername(change.uname, function (results) {
+    if (change.old_pass === results[0].password) {
+      if (change.new_pass === change.c_new_pass) {
+        travel_guide_model.changePassword(change, function (status) {
+          if (status) {
+            res.redirect('/user');
+          } else {
+            res.send('Not Update');
+          }
+        });
+      } else {
+        res.send("Password doesn't Match");
+      }
+    } else {
+      res.send("Old Pass doesn't match");
+    }
+  });
 });
 
 module.exports = router;
